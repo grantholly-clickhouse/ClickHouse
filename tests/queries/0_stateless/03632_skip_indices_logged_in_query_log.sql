41478360fa4bbb55aa7445bfa3f75673b6e7c191
Grant Holly <grant.holly@clickhouse.com>
2026-03-24 11:05:28 -0700

    ensuring we can handle weird table and index names

--- tests/queries/0_stateless/03632_skip_indices_logged_in_query_log.sql
+++ tests/queries/0_stateless/03632_skip_indices_logged_in_query_log.sql
@@ -37,10 +37,26 @@ SELECT * FROM children FORMAT Null SETTINGS log_comment='1'; -- no skip indexes 
 SELECT * FROM children WHERE age = 3 FORMAT Null SETTINGS log_comment='2'; -- age_i used
 SELECT * FROM children WHERE startsWith(name, 'Al') FORMAT Null SETTINGS log_comment='3'; -- name_i used
 SELECT * FROM children WHERE age = 3 AND name = 'Alice' FORMAT Null SETTINGS log_comment='4'; -- age_i and name_i used
-SELECT * FROM children 
-LEFT JOIN parents ON children.parent_id = parents.uid 
-WHERE parents.age >= 25 AND children.name = 'Alice' 
+SELECT * FROM children
+LEFT JOIN parents ON children.parent_id = parents.uid
+WHERE parents.age >= 25 AND children.name = 'Alice'
 FORMAT Null SETTINGS log_comment='5'; -- age_i and name_i used
+
+-- Test with identifiers that require escaping: table "hello, world" and index "my.favorite.index"
+DROP TABLE IF EXISTS `hello, world`;
+CREATE TABLE `hello, world` (
+    `uid` Int16,
+    age Int16,
+    INDEX `my.favorite.index` age TYPE set(10) GRANULARITY 2
+)
+ENGINE = MergeTree()
+ORDER BY `uid`;
+
+INSERT INTO `hello, world` VALUES (1, 30);
+
+SELECT * FROM `hello, world` WHERE age = 30 FORMAT Null SETTINGS log_comment='6'; -- my.favorite.index used
+
+DROP TABLE `hello, world`;
 
 SYSTEM FLUSH LOGS system.query_log;
 
@@ -50,6 +66,6 @@ WHERE
     event_date >= yesterday()
     AND type = 'QueryFinish'
     AND current_database = currentDatabase()
-    AND log_comment in ('1', '2', '3', '4', '5')
-ORDER BY log_comment ASC, event_time DESC 
+    AND log_comment in ('1', '2', '3', '4', '5', '6')
+ORDER BY log_comment ASC, event_time DESC
 LIMIT 1 BY log_comment;
